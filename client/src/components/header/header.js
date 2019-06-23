5d47a6488df37f19b8e079b3084f7f03b11fa4c3
ssalcedo00 <salsalcedo4321@gmail.com>
2019-06-23 12:30:16 -0700

    first header done

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -5,11 +5,21 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import React, { Component } from "react";
 
+const logo = require('../../../public/static/images/white-logo.png');
+
 export default class Header extends Component {
     render() {
         return (
-            <Navbar sticky="top" bg="dark" variant="dark" expand="xl">
-                <Navbar.Brand href="/">mysubs.live</Navbar.Brand>
+            <Navbar sticky="top" variant="dark" expand="xl">
+                <Container>
+                    <Navbar.Brand className="navbar__brand-position" href="/">
+                    <img
+                        alt='mysubs.live Logo'
+                        src={logo}
+                        className="d-inline-block align-middle"
+                        height='20'
+                    />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
@@ -20,8 +30,8 @@ export default class Header extends Component {
                             <Nav.Link>Sign up</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
+                </Container>
             </Navbar>
-           
         );
     }
 }
