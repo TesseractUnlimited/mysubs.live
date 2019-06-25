e37d0212b2d4a4782fd366c3ba8e9dffda3331de
ssalcedo00 <salsalcedo4321@gmail.com>
2019-06-24 18:54:19 -0700

    worked on error page

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -1,12 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './Error.css';
 import React, { Component } from "react";
-import Layout from '../layout/layout';
+import Row  from 'react-bootstrap/Row';
+import Header from '../header/header'
+import Footer from '../footer/footer'
+import Container from 'react-bootstrap/Container';
 
 export default class Error extends Component { 
     render() {
         return (
-            <div>       
-                <h1>404! Error!</h1>    
+            <div>    
+                <Header />
+                <Container className="error-container">
+                    <h1 className="h1-error">Oops!</h1>
+                    <h2 className="h2-error">We can't seem to find the page you're looking for. </h2>
+                    <h2 className="h2-error">Error Code: 404</h2>
+                </Container>
+                <Footer />
             </div>
         );
     }
