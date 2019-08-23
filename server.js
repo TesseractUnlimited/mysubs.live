2593b97623dea43462c3ca421902ba5985dd2f5e
ssalcedo00 <salsalcedo4321@gmail.com>
2019-08-22 21:10:11 -0700

    fuck git and aws

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 // Send index.html when new route appears
 app.get('/*',  (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 // Database Connection
