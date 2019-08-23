18763467a5d3e0b9394d2bb5e0fa95fb69d8b92e
ssalcedo00 <salsalcedo4321@gmail.com>
2019-08-22 19:53:07 -0700

    server changes

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-// route imports 
+// Route Imports 
 const subRoutes = require('./api/routes/sub');
 const subsRoutes = require('./api/routes/subs');
 const userRoutes = require('./api/routes/user');
@@ -17,13 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({ message: message });
-})
+// Database Connection
 const uri = 'mongodb+srv://ssalcedo00:nalgonio1@cluster0-vtosk.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true })
 const connection = mongoose.connection;
@@ -31,13 +24,13 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully.");
 })
 
+// Header Settings
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-
 
 // '/api' GET route
 app.use('/sub', subRoutes);
