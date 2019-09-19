9f963a1ae6bb0e3225b4ad3b64901511992a2147
ssalcedo00 <salsalcedo4321@gmail.com>
2019-09-19 09:57:16 -0700

    stuff?

---
+++ client/src/components/BlurryPopup/BlurryPopup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Form, Button, Card, Row } from "react-bootstrap";
+import './BlurryPopup.css';
+
+export default class Footer extends Component { 
+    render() {
+        return (
+            <div className="blurry-popup">
+                <p>Hello.</p>
+            </div>
+        );
+    }
+}
